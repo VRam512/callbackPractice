3127862e3a8b5cf500f8d7662d915ed5a7136a29
Vicente Ramirez <vram512@gmail.com>
2017-11-29 16:44:18 -0600

    week1 day2 third commit

--- callbackPractice.js
+++ callbackPractice.js
@@ -1,55 +1,49 @@
 /* In this repo your job is to write functions to make each function call work properly.
 Below is a sample problem 
-
-  //code here for sayHi
-
-   sayHi('Hi Katie', function(thingToSay){
-      alert(thingToSay);
-   });
-   
+  
 and WHAT YOU SHOULD WRITE is the sayHi function that makes the code above work: 
     
-   var sayHi = function(str, cb){
-    cb(str);
-   }
+   var sayHi = function(str, cb){cb(str);}
+
+     //code here for sayHi
+
+   sayHi('Hi Katie', function(thingToSay){alert(thingToSay);});
     
 */
 
 
-// 1. Write a function called first that takes in two parameters, an array and a callback function, then invokes the callback function, passing in the first element in the array as it's argument.  (see the sayHi function on line 12 for reference)
+// 1. Write a function called first that takes in two parameters, an array and a callback function, then invokes the callback function, 
+// passing in the first element in the array as it's argument.  (see the sayHi function on line 12 for reference)
 
   // Code Here 
 
+var first = function(arr,cb){cb(arr[0]);};
+//function first(arr,cb){cb(arr[0]);};             <<<<===== function can be written this way too
   
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
 
-first(names, function(firstName){
-  console.log('The first name in names is ' + firstName);
-  return firstName;
-});
+first(names, function(firstName){console.log('The first name in names is ' + firstName);return firstName;});
 
 
 
 // 2. Write a function called last that takes in an array and a callback function, then invokes the callback, passing in the last element in the array as the argument.
 
+  //Code here
+  var last = function(arr,cb){cb(arr[arr.length - 1]);};
+
+
+last(names, function(lastName){console.log('The last name in names is ' + lastName); return lastName;});
+
+
+
+// 3. Write a function called multiply that takes in three parameters: two numbers and a callback function.  Invoke the callback, passing in the product 
+//of the two numbers multiplied as the argument. 
+
   //Code Here
+var multiply = function (n1,n2,cb){cb(n1*n2);};
 
 
-last(names, function(lastName){
-  console.log('The last name in names is ' + lastName);
-  return lastName;
-});
-
-
-
-// 3. Write a function called multiply that takes in three parameters: two numbers and a callback function.  Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
-
-  //Code Here
-
-
-multiply(4, 3, function(answer){
-  console.log('The answer is ' + answer); //should console.log 12
-})
+multiply(4, 3, function(answer){console.log('The answer is ' + answer);})
 
 
 
@@ -59,7 +53,10 @@ multiply(4, 3, function(answer){
 
   //Code Here 
 
-
+var contains = function (arr,name,cb){
+  if (arr.indexOf(name) === 0) {cb(true)}
+  else {cb(false)}
+}
 
 
 contains(names, 'Colt', function(result){
@@ -76,7 +73,29 @@ contains(names, 'Colt', function(result){
 // Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 
   //Code Here
+// var uniq = function(arr,cb){
+    
+//   for (var i = arr.length -1 ; i >= 0; i--){
+//     for (var j = i-1; j>=0;j--){
+//       if (arr[i] == arr[j]){
+//         arr.splice(j,1);
+//       }
+//     }
+//   }return arr;
+// };
 
+
+const uniq = (arr, cb) => {
+  // let sorted = arr.slice().sort();
+  let sorted = arr.sort();
+  for(var i = 0; i < sorted.length - 1; i++) {
+    if(sorted[i+1] === sorted[i]) {
+      sorted.splice(i, 1);
+      i--
+    }
+  }
+  cb(sorted)
+}
 
 
 uniq(names, function(uniqArr){
@@ -84,10 +103,18 @@ uniq(names, function(uniqArr){
 });
 
 
-// 6. Write a function called each that takes in an array of names and a callback function. For each name in the array, invoke the callback and pass in the name and the name's index as arguments.
+// 6. Write a function called each that takes in an array of names and a callback function. For each name in the array, invoke the callback 
+// and pass in the name and the name's index as arguments.
 
     //Code Here 
 
+//   var each = (arr,cb) => {cb(arr.forEach(name,idx))};
+
+
+    var each = (arr,cb) => { 
+      for (var i = 0; i < arr.length ; i++) {
+        cb(arr[i],i);        
+      }};
 
 
 each(names, function(item, indice){
@@ -100,6 +127,13 @@ each(names, function(item, indice){
 // When the correct user object is found, invoke the callback with the user object as an argument.
 
 // Code here
+var getUserById = (arr,id,cb) => {
+  for (var i = 0; i < arr.length; i++)
+    if (arr[i].id === id){
+      cb(arr[i])
+    }
+};
+
 
 
 
